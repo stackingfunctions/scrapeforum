26a6fdf327185c0d475524bf90222591b377966d
Gergo <stackingfunctions@users.noreply.github.com>
2016-09-25 22:26:07 +0200

    Started implementation of MyLogger. It could be modified by adding DB logging for certain log levels.

--- python/src/mylogger.py
+++ python/src/mylogger.py
@@ -1,16 +1,37 @@
-from lastbatch import LastBatch
-
-#lastBatch = LastBatch()
-#for topic in lastBatch.getProcessedSet():
-#    print(topic)
-
-
-
+import os
 import logging.config
 
-logging.config.fileConfig('../config/logging.conf')
+class MyLogger(object):
 
-# create logger
-logger = logging.getLogger('scrapeforum')
-logger.addHandler(logging.StreamHandler())
+    # set logging to both file and screen
+    def __init__(self):
+        logging.config.fileConfig('../config/logging.conf')
+        self.logger = logging.getLogger('scrapeforum')
+        self.logger.addHandler(logging.StreamHandler())
+        self.errorIndicated = False
+
+    def isErrorIndicated(self):
+        return self.errorIndicated
+
+    def debug(self, msg):
+        self.logger.debug(msg)
+
+    def info(self, msg):
+        self.logger.info(msg)
+
+    def warning(self, msg):
+        self.logger.warning(msg)
+
+    def error(self, msg):
+        self.logger.error(msg)
+        self.errorIndicated = True
+
+    def critical(self, msg):
+        self.logger.critical(msg)
+        self.errorIndicated = True
+
+    def fatal(self, msg):
+        self.logger.fatal(msg)
+        self.errorIndicated = True
+
 
